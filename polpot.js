0a78938406be144bdfde9df3a263a779eca0a4de
TanodSikio <151496101+TanodSikio@users.noreply.github.com>
2023-12-05 22:49:05 +0800

    Add files via upload

---
+++ polpot.js
@@ -0,0 +1,167 @@
+function checkGuess() {
+    var guessInput = document.getElementById('guess-input').value;
+    var resultMessage = document.getElementById('result-message');
+
+
+    var correctAnswer = 'ball';
+
+    if (guessInput === correctAnswer) {
+        resultMessage.textContent = 'Congratulations! Your answer is correct.';
+    } else if (guessInput === '') {
+        resultMessage.textContent = 'Please enter a guess.';
+    } else {
+        resultMessage.textContent = 'Sorry, your answer is incorrect. Try again.';
+    }
+}
+
+function checkGuesse() {
+    var guessInput = document.getElementById('guess-inputs').value;
+    var resultMessage = document.getElementById('result-messages');
+
+
+    var correctAnswer = 'pair';
+
+    if (guessInput === correctAnswer) {
+        resultMessage.textContent = 'Congratulations! Your answer is correct.';
+    } else if (guessInput === '') {
+        resultMessage.textContent = 'Please enter a guess.';
+    } else {
+        resultMessage.textContent = 'Sorry, your answer is incorrect. Try again.';
+    }
+}
+
+function checkGuesses() {
+    var guessInput = document.getElementById('guess-inputss').value;
+    var resultMessage = document.getElementById('result-messagess');
+
+
+    var correctAnswer = 'mouse';
+
+    if (guessInput === correctAnswer) {
+        resultMessage.textContent = 'Congratulations! Your answer is correct.';
+    } else if (guessInput === '') {
+        resultMessage.textContent = 'Please enter a guess.';
+    } else {
+        resultMessage.textContent = 'Sorry, your answer is incorrect. Try again.';
+    }
+}
+function checkGuesser() {
+    var guessInput = document.getElementById('guess-inputsy').value;
+    var resultMessage = document.getElementById('result-messager');
+
+
+    var correctAnswer = 'happy';
+
+    if (guessInput === correctAnswer) {
+        resultMessage.textContent = 'Congratulations! Your answer is correct.';
+    } else if (guessInput === '') {
+        resultMessage.textContent = 'Please enter a guess.';
+    } else {
+        resultMessage.textContent = 'Sorry, your answer is incorrect. Try again.';
+    }
+}
+function wish() {
+    var guessInput = document.getElementById('guess-wish').value;
+    var resultMessage = document.getElementById('result-wish');
+
+
+    var correctAnswer = 'wish';
+
+    if (guessInput === correctAnswer) {
+        resultMessage.textContent = 'Congratulations! Your answer is correct.';
+    } else if (guessInput === '') {
+        resultMessage.textContent = 'Please enter a guess.';
+    } else {
+        resultMessage.textContent = 'Sorry, your answer is incorrect. Try again.';
+    }
+}
+function wood() {
+    var guessInput = document.getElementById('guess-wood').value;
+    var resultMessage = document.getElementById('result-wood');
+
+
+    var correctAnswer = 'wood';
+
+    if (guessInput === correctAnswer) {
+        resultMessage.textContent = 'Congratulations! Your answer is correct.';
+    } else if (guessInput === '') {
+        resultMessage.textContent = 'Please enter a guess.';
+    } else {
+        resultMessage.textContent = 'Sorry, your answer is incorrect. Try again.';
+    }
+}
+function work() {
+    var guessInput = document.getElementById('guess-work').value;
+    var resultMessage = document.getElementById('result-work');
+
+
+    var correctAnswer = 'work';
+
+    if (guessInput === correctAnswer) {
+        resultMessage.textContent = 'Congratulations! Your answer is correct.';
+    } else if (guessInput === '') {
+        resultMessage.textContent = 'Please enter a guess.';
+    } else {
+        resultMessage.textContent = 'Sorry, your answer is incorrect. Try again.';
+    }
+}
+function wrap() {
+    var guessInput = document.getElementById('guess-wrap').value;
+    var resultMessage = document.getElementById('result-wrap');
+
+
+    var correctAnswer = 'wrap';
+
+    if (guessInput === correctAnswer) {
+        resultMessage.textContent = 'Congratulations! Your answer is correct.';
+    } else if (guessInput === '') {
+        resultMessage.textContent = 'Please enter a guess.';
+    } else {
+        resultMessage.textContent = 'Sorry, your answer is incorrect. Try again.';
+    }
+}
+function christmas() {
+    var guessInput = document.getElementById('guess-christmas').value;
+    var resultMessage = document.getElementById('result-christmas');
+
+
+    var correctAnswer = 'christmas';
+
+    if (guessInput === correctAnswer) {
+        resultMessage.textContent = 'Congratulations! Your answer is correct.';
+    } else if (guessInput === '') {
+        resultMessage.textContent = 'Please enter a guess.';
+    } else {
+        resultMessage.textContent = 'Sorry, your answer is incorrect. Try again.';
+    }
+}
+function yawn() {
+    var guessInput = document.getElementById('guess-yawn').value;
+    var resultMessage = document.getElementById('result-yawn');
+
+
+    var correctAnswer = 'yawn';
+
+    if (guessInput === correctAnswer) {
+        resultMessage.textContent = 'Congratulations! Your answer is correct.';
+    } else if (guessInput === '') {
+        resultMessage.textContent = 'Please enter a guess.';
+    } else {
+        resultMessage.textContent = 'Sorry, your answer is incorrect. Try again.';
+    }
+}
+function zero() {
+    var guessInput = document.getElementById('guess-zero').value;
+    var resultMessage = document.getElementById('result-zero');
+
+
+    var correctAnswer = 'zero';
+
+    if (guessInput === correctAnswer) {
+        resultMessage.textContent = 'Congratulations! Your answer is correct.';
+    } else if (guessInput === '') {
+        resultMessage.textContent = 'Please enter a guess.';
+    } else {
+        resultMessage.textContent = 'Sorry, your answer is incorrect. Try again.';
+    }
+}
